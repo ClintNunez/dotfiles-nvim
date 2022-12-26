24ce6e19106c599959a7aae1c3d6ad6872aa9dc5
ClintNunez <clintnunez14@gmail.com>
2022-12-26 17:42:54 +0800

    customized the colors

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -2,32 +2,91 @@ local status, bufferline = pcall(require, "bufferline")
 if (not status) then return end
 
 bufferline.setup({
-  options = {
-    mode = "tabs",
-    separator_style = 'slant',
-    always_show_bufferline = false,
-    show_buffer_close_icons = false,
-    show_close_icon = false,
-    color_icons = true
-  },
-  highlights = {
-    separator = {
-      fg = '#073642',
-      bg = '#002b36',
+    options = {
+        mode = "tabs",
+        separator_style = 'slant',
+        always_show_bufferline = false,
+        show_buffer_close_icons = true,
+        show_close_icon = false,
+        color_icons = true
     },
-    separator_selected = {
-      fg = '#073642',
+    highlights = {
+        separator = {
+            fg = '#434852',
+            bg = '#333C47'
+        },
+        separator_selected = {
+            fg = '#434852',
+        },
+        close_button = {
+            fg = '#657b83',
+            bg = '#333C47',
+        },
+        close_button_selected = {
+            fg = '#F0FDFF',
+        },
+        hint = {
+            fg = '#657b83',
+            bg = '#333C47'
+        },
+        diagnostic = {
+            fg = '#657b83',
+            bg = '#333C47'
+        },
+        info = {
+            fg = '#657b83',
+            bg = '#333C47'
+        },
+        info_diagnostic = {
+            fg = '#657b83',
+            bg = '#333C47'
+        },
+        warning = {
+            fg = '#FFd17f',
+            bg = '#333C47'
+        },
+        warning_selected = {
+            fg = '#FFd17f',
+        },
+        warning_diagnostic = {
+            fg = '#FFd17f',
+            bg = '#333C47'
+        },
+        warning_diagnostic_selected = {
+            fg = '#FFd17f',
+        },
+        error = {
+            fg = '#C53B3A',
+            bg = '#333C47'
+        },
+        error_selected = {
+            fg = '#C53B3A',
+        },
+        error_diagnostic = {
+            fg = '#C53B3A',
+            bg = '#333C47'
+        },
+        error_diagnostic_selected = {
+            fg = '#C53B3A',
+        },
+        modified = {
+            fg = '#657b83',
+            bg = '#333C47'
+        },
+        duplicate = {
+            fg = '#657b83',
+            bg = '#333C47'
+        },
+        background = {
+            fg = '#657b83',
+            bg = '#333C47'
+        },
+        buffer_selected = {
+            fg = '#F0FDFF',
+            bold = true,
+        },
+        fill = {
+            bg = '#434852'
+        }
     },
-    background = {
-      fg = '#657b83',
-      bg = '#002b36'
-    },
-    buffer_selected = {
-      fg = '#fdf6e3',
-      bold = true,
-    },
-    fill = {
-      bg = '#073642'
-    }
-  },
 })
