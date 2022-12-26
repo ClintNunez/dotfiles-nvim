a95d0ea9dac93c4e1e89b5dea535bb4e96d832f6
ClintNunez <clintnunez14@gmail.com>
2022-12-26 11:26:14 +0800

    initial commit

---
+++ lua/custom/set.lua
@@ -0,0 +1,39 @@
+-- Leader
+vim.g.mapleader = " "
+
+-- Left Bar
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+
+-- Tabs
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Searching
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+-- Error
+vim.opt.errorbells = false
+
+-- Undo
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = "~/.vim/undodir"
+vim.opt.undofile = true
+
+-- Scrolling
+vim.opt.wrap = false
+vim.opt.scrolloff = 12
+vim.opt.sidescrolloff = 8
+
+-- Color
+-----For oceanic-material
+vim.opt.background = "dark"
+vim.opt.termguicolors = true
+
+vim.opt.updatetime = 50
