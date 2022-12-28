01ecd1476f960b9df9b32c7a760f6a900ce97afd
ClintNunez <clintnunez14@gmail.com>
2022-12-28 12:55:06 +0800

    added other configs

---
+++ fish/conf.d/tide.fish
@@ -0,0 +1,45 @@
+# dir
+set -g tide_pwd_bg_color 434852 
+set -g tide_pwd_color_dirs FEEEED 
+set -g tide_pwd_color_anchors FEEEED 
+set -g tide_pwd_color_truncated_dirs FEEEED 
+
+# os
+set -g tide_os_bg_color 6D7B8B 
+set -g tide_os_color FEEEED 
+
+# time
+set -g tide_time_bgcolor 434852  
+set -g tide_time_color FEEEED
+
+# status
+set -g tide_status_bgcolor 434852 
+set -g tide_status_bgcolor_failure 434852 
+set -g tide_status_color 68BC5F
+set -g tide_status_color_failure CE5A59
+
+#cmd_duration
+set -g tide_cmd_duration_bg_color 434852
+set -g tide_cmd_duration_color FEEEED
+
+# git
+set -g tide_git_bg_color 268BD2
+set -g tide_git_bg_color_unstable C4A000
+set -g tide_git_bg_color_urgent CC0000
+set -g tide_git_branch_color 000000
+set -g tide_git_color_branch 000000
+set -g tide_git_color_conflicted 000000
+set -g tide_git_color_dirty 000000
+set -g tide_git_color_operation 000000
+set -g tide_git_color_staged 000000
+set -g tide_git_color_stash 000000
+set -g tide_git_color_untracked 000000
+set -g tide_git_color_upstream 000000
+set -g tide_git_conflicted_color 000000
+set -g tide_git_dirty_color 000000
+set -g tide_git_icon 
+set -g tide_git_operation_color 000000
+set -g tide_git_staged_color 000000
+set -g tide_git_stash_color 000000
+set -g tide_git_untracked_color 000000
+set -g tide_git_upstream_color 000000
