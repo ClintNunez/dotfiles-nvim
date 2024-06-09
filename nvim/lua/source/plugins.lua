b8d10751427851aa68f22cf7ee1cf76eb7e1771c
ClintNunez <clintnunez14@gmail.com>
2024-06-09 22:18:05 +0800

    added surround and organized the plugins

--- nvim/lua/source/plugins.lua
+++ nvim/lua/source/plugins.lua
@@ -6,13 +6,18 @@ require("lazy").setup({
         lazy = false,
         priority = 1000,
     },
+    -- text colors
     {
-        "nvimtools/none-ls.nvim",
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
     },
-    {
+    "norcalli/nvim-colorizer.lua",
+
+    {-- fuzzyfinder
         "nvim-telescope/telescope.nvim", tag = "0.1.5",
         dependencies = { "nvim-lua/plenary.nvim" }
     },
+
     -- LSP
     { "VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
     "williamboman/mason.nvim",
@@ -24,24 +29,35 @@ require("lazy").setup({
         "L3MON4D3/LuaSnip",
         dependencies = { "rafamadriz/friendly-snippets" },
     },
-    "norcalli/nvim-colorizer.lua",
     {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" }
+        "nvimtools/none-ls.nvim",
     },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-    },
-    {
+
+    { -- tabs
         "akinsho/bufferline.nvim",
         version = "*",
         dependencies = { "nvim-tree/nvim-web-devicons" }
     },
+    { -- better statusline
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" }
+    },
+
+    -- ease of life
     {
         "windwp/nvim-autopairs",
         event = "InsertEnter",
     },
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    }
 })
 
 require("luasnip.loaders.from_vscode").lazy_load()
