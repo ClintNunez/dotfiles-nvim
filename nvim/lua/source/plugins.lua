cd782c98f51d3a7c4e788ff21eeadc54d872215d
ClintNunez <clintnunez14@gmail.com>
2024-06-11 15:59:43 +0800

    added neogit

--- nvim/lua/source/plugins.lua
+++ nvim/lua/source/plugins.lua
@@ -1,9 +1,43 @@
 require("lazy").setup({
     -- Github
     {
-
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = true
     },
     "lewis6991/gitsigns.nvim",
+
+    {-- fuzzyfinder
+        "nvim-telescope/telescope.nvim", tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" }
+    },
+
+    -- LSP
+    { "VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/nvim-cmp",
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = { "rafamadriz/friendly-snippets" },
+    },
+    "nvimtools/none-ls.nvim",
+
+    { -- tabs
+        "akinsho/bufferline.nvim",
+        version = "*",
+        dependencies = { "nvim-tree/nvim-web-devicons" }
+    },
+    { -- better statusline
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" }
+    },
+
     { -- theme
         "neanias/everforest-nvim",
         name = "everforest",
@@ -42,36 +76,6 @@ require("lazy").setup({
                 perf = { "#9DA9A0" }
             }
         },
-    },
-
-    {-- fuzzyfinder
-        "nvim-telescope/telescope.nvim", tag = "0.1.5",
-        dependencies = { "nvim-lua/plenary.nvim" }
-    },
-
-    -- LSP
-    { "VonHeikemen/lsp-zero.nvim", branch = "v3.x"},
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/nvim-cmp",
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = { "rafamadriz/friendly-snippets" },
-    },
-    {
-        "nvimtools/none-ls.nvim",
-    },
-
-    { -- tabs
-        "akinsho/bufferline.nvim",
-        version = "*",
-        dependencies = { "nvim-tree/nvim-web-devicons" }
-    },
-    { -- better statusline
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" }
     },
 
     -- ease of life
