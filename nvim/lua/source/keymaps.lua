1132ebfc9d47b948714acbf8dac807ab284e2a7d
ClintNunez <clintnunez14@gmail.com>
2024-06-11 19:09:35 +0800

    changed some keymaps

--- nvim/lua/source/keymaps.lua
+++ nvim/lua/source/keymaps.lua
@@ -5,8 +5,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>vp", vim.cmd.Ex)
 
 -- Increment / Decrement
-vim.keymap.set("n", "+", "<C-a>")
-vim.keymap.set("n", "-", "<C-x>")
+vim.keymap.set("n", ">", "<C-a>")
+vim.keymap.set("n", "<", "<C-x>")
 
 -- Select all
 vim.keymap.set("n", "<C-a>", "gg<S-v>G")
@@ -19,27 +19,24 @@ vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {})
 vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
 
 -- Split window
-vim.keymap.set("n", "ss", ":split<Return><C-w>w")
+vim.keymap.set("n", "sh", ":split<Return><C-w>w")
 vim.keymap.set("n", "sv", ":vsplit<Return><C-w>w")
 
 -- Move to other window
-vim.keymap.set("", "sh", "<C-w>h")
-vim.keymap.set("", "sk", "<C-w>k")
-vim.keymap.set("", "sj", "<C-w>j")
-vim.keymap.set("", "sl", "<C-w>l")
+vim.keymap.set("", "wh", "<C-w>h")
+vim.keymap.set("", "wk", "<C-w>k")
+vim.keymap.set("", "wj", "<C-w>j")
+vim.keymap.set("", "wl", "<C-w>l")
 
 -- Resize window
-vim.keymap.set("n", "<C-s>h", "<C-w><")
-vim.keymap.set("n", "<C-s>l", "<C-w>>")
-vim.keymap.set("n", "<C-s>k", "<C-w>+")
-vim.keymap.set("n", "<C-s>j", "<C-w>-")
+vim.keymap.set("n", "rh", "<C-w><")
+vim.keymap.set("n", "rl", "<C-w>>")
+vim.keymap.set("n", "rk", "<C-w>+")
+vim.keymap.set("n", "rj", "<C-w>-")
 
 -- Moves blocks of code in Visual Mode
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
--- Cursor stays in place after pressing J
-vim.keymap.set("n", "J", "mzJ`z")
 
 -- Cursor stays in the middle while performing half page jumping
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -49,8 +46,8 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
--- <Space>p paste over words without copying
-vim.keymap.set("x", "<leader>p", [["_dP]])
+-- remap p to paste over words without copying
+vim.keymap.set("x", "p", [["_dP]])
 
 -- Deletes texts without copying
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
@@ -63,7 +60,3 @@ vim.keymap.set("n", "Q", "<nop>")
 
 -- Replaces all occurences of the word that the cursor is on
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-
--- Makes bash scripts executable (?)
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
-
