3c74d3b59c80dddd66e7d4380e9165e8b097d91f
ClintNunez <clintnunez14@gmail.com>
2024-06-13 19:03:36 +0800

    added more keymaps to use more functions

--- nvim/lua/source/plugins_config/telescope.lua
+++ nvim/lua/source/plugins_config/telescope.lua
@@ -1,7 +1,18 @@
 local builtin = require('telescope.builtin')
+
+-- files and strings
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+-- help
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>fw', function()
-    builtin.grep_string({ search = vim.fn.input("Grep > ") })
-end)
+
+-- quickfix
+vim.keymap.set('n', '<leader>fqf', builtin.quickfix, {})
+
+-- lsp
+vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
+
+-- git
+vim.keymap.set('n', '<leader>fgc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>fgs', builtin.git_status, {})
